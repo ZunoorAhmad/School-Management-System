024296bc127159f707a628d1d04010400778f81f
Zunoor Ahmad <zunoorahmad.3ss@gmail.com>
2025-04-13 20:32:55 +0500

    Initial commit

---
+++ environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    backend: {
+      // baseURL: "https://api.deabt.us/",
+      baseURL: "http://127.0.0.1:8000/",
+      api: "api/",
+    },
+  };
+
